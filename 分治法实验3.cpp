84cbc2a273432e563d555ee558de9b35f23027ee
Donald-li <1026619991@qq.com>
2019-11-24 21:39:37 +0800

    实验3，递归第一次提交，未完成

--- 分治法实验3.cpp
+++ 分治法实验3.cpp
@@ -1,7 +1,49 @@
 #include<stdio.h>
 int a[128][128];
 
-void Table(int k){
+void Table(int left,int right){
+	
+	int mid = (right-left)/2;
+	if((right-left+1)>2){
+		Table(left,mid);
+		Table(mid+1,right);
+	}else{
+		for(int i = left;i<right;i++){
+			for(int j = left;j<right;j++){
+				
+			}
+		}
+	}
+	
+
+	//按照球员规模配置赛程表 
+	int m = 1;
+	
+	for(int s = 1;s<=k;s++){
+		
+		n/=2;
+		for(int t=1;t<=n;t++){
+			
+			for(int i=m+1;i<=2*m;i++){
+				for(int j = m+1;j<=2*m;j++){
+					
+					a[i][j+(t-1)*m*2]=a[i-m][j+(t-1)*m*2-m];
+					a[i][j+(t-1)*m*2-m]=a[i-m][j+(t-1)*m*2];
+				}
+				m*=2;
+			}
+		}
+	}
+	
+} 
+
+
+int main(){
+	
+	int k=0;
+	printf("请输入球队规模（2的k次幂？k<7）：");
+	scanf("%d",&k);
+	
 	//k为所有球员数量的幂，借此求出球员数量n。 
 	int n = 1;
 	for(int i = 1;i<=k;i++){
@@ -11,36 +53,16 @@ void Table(int k){
 	for(int i=1;i<=n;i++){
 		a[1][i] = i;
 	}
-	//按照球员规模配置赛程表 
-	int m = 1;
-	for(int s = 1;s<=k;s++){
-		n/=2;
-		for(int t=1;t<=n;t++){
-			for(int i=m+1;i<=2*m;i++){
-				for(int j = m+1;j<=2*m;j++){
-					a[i][j+(t-1)*m*2]=a[i-m][j+(t-1)*m*2-m];
-					a[i][j+(t-1)*m*2-m]=a[i-m][j+(t-1)*m*2];
-				}
-				m*=2;
-			}
-		}
-	}
-
+	
+	Table(k);
+	
 	for(int i = 0;i<n;i++){
 		for(int j=0;j<n;j++){
 			printf("%d",a[i][j]);
-			printf("\n");
+			
 		}
+		printf("\n");
 	}	
-} 
-
-
-int main(){
-	
-	int k=0;
-	printf("请输入球队规模（2的k次幂？k<7）：");
-	scanf("%d",&k);
-	Table(k);
 	
 	return 0;
 }
