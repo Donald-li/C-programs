e8bc0748b91f1199abda6ffed63b9bb74b3f497e
Donald-li <1026619991@qq.com>
2019-11-24 23:51:26 +0800

    实验三，递归方式，缺陷提交

--- 分治法实验3.cpp
+++ 分治法实验3.cpp
@@ -1,64 +1,70 @@
 #include<stdio.h>
 int a[128][128];
-
+int b[128];
+int k=0;
+int n = 1;
 void Table(int left,int right){
 	
-	int mid = (right-left)/2;
+	int mid = (right+left)/2;
 	if((right-left+1)>2){
-		Table(left,mid);
 		Table(mid+1,right);
-	}else{
-		for(int i = left;i<right;i++){
-			for(int j = left;j<right;j++){
+		Table(left,mid);
+		
+		//将左下角的部分赋给右上角 
+		for(int i=0;i<=(right-left)/2;i++){
+			for(int j=0;j<=(right-left)/2;j++){
+				a[mid+1+i][j]=a[i+left][(right-left+1)/2+j];
+			}
+		}
+		//将左上角的部分赋给右下角 
+		for(int i=0;i<=(right-left)/2;i++){
+			for(int j=0;j<=(right-left)/2;j++){
+				a[mid+1+i][(right-left+1)/2+j]=a[left+i][j];
 				
 			}
 		}
-	}
-	
-
-	//按照球员规模配置赛程表 
-	int m = 1;
-	
-	for(int s = 1;s<=k;s++){
 		
-		n/=2;
-		for(int t=1;t<=n;t++){
-			
-			for(int i=m+1;i<=2*m;i++){
-				for(int j = m+1;j<=2*m;j++){
-					
-					a[i][j+(t-1)*m*2]=a[i-m][j+(t-1)*m*2-m];
-					a[i][j+(t-1)*m*2-m]=a[i-m][j+(t-1)*m*2];
+	}else{
+			for(int i = left;i<=right;i++){
+				for(int j = left;j<=right;j++){
+					if(i!=j){
+						a[1][i] = b[j];
+					}else{
+						continue;
+					}
 				}
-				m*=2;
 			}
-		}
 	}
-	
 } 
 
 
 int main(){
 	
-	int k=0;
+
 	printf("请输入球队规模（2的k次幂？k<7）：");
 	scanf("%d",&k);
 	
 	//k为所有球员数量的幂，借此求出球员数量n。 
-	int n = 1;
+
 	for(int i = 1;i<=k;i++){
 		n*=2;
 	}
+	
+	//初始化b，代表队员编号
+	for(int i = 1;i<=n;i++){
+		b[i-1] = i;
+	} 
+	
 	//初始化第一列，用第一列的序号代表球员序号 
-	for(int i=1;i<=n;i++){
-		a[1][i] = i;
+	for(int i=0;i<n;i++){
+		a[0][i] = i+1;
 	}
 	
-	Table(k);
+	Table(0,n-1);
 	
 	for(int i = 0;i<n;i++){
 		for(int j=0;j<n;j++){
-			printf("%d",a[i][j]);
+			printf("%d",a[j][i]);
 			
 		}
 		printf("\n");
