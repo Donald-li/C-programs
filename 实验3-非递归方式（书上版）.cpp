78b54a9f8914fc0fd1ad1541d5a848d19b42dce2
Donald-li <1026619991@qq.com>
2019-11-25 21:20:10 +0800

    实验三，最后提交

--- 实验3-非递归方式（书上版）.cpp
+++ 实验3-非递归方式（书上版）.cpp
@@ -8,29 +8,31 @@ void Table(int k){
 		n*=2;
 	}
 	//初始化第一列，用第一列的序号代表球员序号 
-	for(int i=1;i<=n;i++){
-		a[1][i] = i;
+	for(int i=0;i<n;i++){
+		a[i][0] = i+1;
 	}
 	//按照球员规模配置赛程表 
 	int m = 1;
 	for(int s = 1;s<=k;s++){
-		n/=2;
+		n=n/2;
 		for(int t=1;t<=n;t++){
 			for(int i=m+1;i<=2*m;i++){
 				for(int j = m+1;j<=2*m;j++){
 					a[i][j+(t-1)*m*2]=a[i-m][j+(t-1)*m*2-m];
 					a[i][j+(t-1)*m*2-m]=a[i-m][j+(t-1)*m*2];
 				}
-				m*=2;
+				m=m*2;
 			}
 		}
 	}
 
+	//输出赛程数组 
 	for(int i = 0;i<n;i++){
 		for(int j=0;j<n;j++){
 			printf("%d",a[i][j]);
-			printf("\n");
+			
 		}
+		printf("\n");
 	}	
 } 
 
@@ -41,6 +43,6 @@ int main(){
 	printf("请输入球队规模（2的k次幂？k<7）：");
 	scanf("%d",&k);
 	Table(k);
-	
+
 	return 0;
 }
