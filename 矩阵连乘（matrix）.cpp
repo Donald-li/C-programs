f1f94285b1f0f3a21d92ead1fd6a673d0d114966
Doanld-li <1026619991@qq.com>
2019-12-05 20:22:01 +0800

    矩阵连乘动态规划有误版1

--- 矩阵连乘（matrix）.cpp
+++ 矩阵连乘（matrix）.cpp
@@ -2,6 +2,8 @@
 
 //用来储存数组的行列信息，第一层表示矩阵数，第二层表示此个矩阵的行列数 
 int matrix[50];
+//用来储存已计算过的结果
+int used[50][50];
 
  //计算两个矩阵相乘所需次数
 int MatrixMultiply(int num1,int num2,int num3){
@@ -12,7 +14,11 @@ int MatrixMultiply(int num1,int num2,int num3){
 
 //递归计算最小相乘次数
 int ReturnMatrixChain(int i,int j){
-	if(i==j) return 0;
+	if(used[i][j]!=-1) return used[i][j];
+	else if(i==j){
+		used[i][j] = 0;
+		return 0;
+	}
 	int u = ReturnMatrixChain(i,i)+ReturnMatrixChain(i+1,j)+MatrixMultiply(matrix[i],matrix[i+1],matrix[j]);
 	for(int k=i+1;k<j;k++){
 		int t = ReturnMatrixChain(i,k)+ReturnMatrixChain(k+1,j)+MatrixMultiply(matrix[i],matrix[k],matrix[j]);
@@ -20,10 +26,18 @@ int ReturnMatrixChain(int i,int j){
 			u=t;
 		}
 	}
+	used[i][j] = u;
 	return u;	
 }
 
 int main(){
+	
+	//初始化记录用数组，未经计算的记为-1 
+	for(int i=0;i<50;i++){
+		for(int j=0;j<50;j++){
+			used[i][j] = -1;
+		}
+	}
 	
 	int n;
 	printf("请输入准备输入的矩阵数：");
